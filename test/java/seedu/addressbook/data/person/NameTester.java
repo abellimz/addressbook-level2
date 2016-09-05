ea231f89c9169c21adef50437d5ebf2d0897ebe1
abellimz <abellim1309@gmail.com>
2016-09-06 00:15:53 +0800

    Add test cases for false positives to NameTester

--- test/java/seedu/addressbook/data/person/NameTester.java
+++ test/java/seedu/addressbook/data/person/NameTester.java
@@ -13,4 +13,18 @@ public class NameTester {
 		Name nameB = new Name("AbelLimJunHong");
 		Assert.assertTrue(nameA.isSimilar(nameB));
 	}
+	
+	@Test
+	public void isSimilar_differentNames_returnFalse() throws IllegalValueException{
+		Name nameA = new Name("Adam Lum Jing Tong");
+		Name nameB = new Name("AbelLimJunHong");
+		Assert.assertFalse(nameA.isSimilar(nameB));
+	}
+	
+	@Test
+	public void isSimilar_subsetName_returnFalse() throws IllegalValueException{
+		Name nameA = new Name("Abel Lim Jun Hong");
+		Name nameB = new Name("Abel Lim");
+		Assert.assertFalse(nameA.isSimilar(nameB));
+	}
 }
