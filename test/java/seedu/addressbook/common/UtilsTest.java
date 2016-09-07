5a4e30bf4b0b91b30b37d12de6b3d74cb311d069
abellimz <abellim1309@gmail.com>
2016-09-07 14:50:08 +0800

    Fix indentation in UtilsTest JUnitTest

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -11,35 +11,35 @@ public class UtilsTest {
 	
 	@Test 
 	public void isAnyNull_allNull_returnTrue(){
-		Object[] testArray = new Object[]{null, null, null, null};
+	    Object[] testArray = new Object[]{null, null, null, null};
 		Assert.assertTrue(Utils.isAnyNull(testArray));
 	}
 	
 	@Test 
 	public void isAnyNull_noNulls_returnFalse(){
-		Object[] testArray = new Object[]{new Object(), new Object()};
-		Assert.assertFalse(Utils.isAnyNull(testArray));
+	    Object[] testArray = new Object[]{new Object(), new Object()};
+	    Assert.assertFalse(Utils.isAnyNull(testArray));
 	}
 	
 	@Test 
 	public void elementsAreUnique_singleElement_returnTrue(){
-		ArrayList<Object> testList = new ArrayList<>();
-		testList.add(1);
-		Assert.assertTrue(Utils.elementsAreUnique(testList));
+	    ArrayList<Object> testList = new ArrayList<>();
+	    testList.add(1);
+	    Assert.assertTrue(Utils.elementsAreUnique(testList));
 	}
 	
 	@Test 
 	public void elementsAreUnique_duplicateElements_returnFalse(){
-		Object object = new Object();
-		Object[] testArray = new Object[]{object, object};
-		List<Object> testList = Arrays.asList(testArray);
-		Assert.assertFalse(Utils.elementsAreUnique(testList));
+	    Object object = new Object();
+	    Object[] testArray = new Object[]{object, object};
+	    List<Object> testList = Arrays.asList(testArray);
+	    Assert.assertFalse(Utils.elementsAreUnique(testList));
 	}
 	
 	@Test 
 	public void elementsAreUnique_duplicateValuesDifferentObjects_returnFalse(){
-		Object[] testArray = new Object[]{new Integer(1), new Integer(1)};
-		List<Object> testList = Arrays.asList(testArray);
-		Assert.assertFalse(Utils.elementsAreUnique(testList));
+	    Object[] testArray = new Object[]{new Integer(1), new Integer(1)};
+	    List<Object> testList = Arrays.asList(testArray);
+	    Assert.assertFalse(Utils.elementsAreUnique(testList));
 	}
 }
